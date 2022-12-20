bf38c708064687f78079b750a7d05a15fc60aad1
ed <erineia@live.com>
2022-12-20 08:50:05 -0300

    buildAccount

--- index.js
+++ index.js
@@ -31,5 +31,38 @@ function operation(){
 //criando conta
 function createAccount(){
       console.log(chalk.bgGreen.black("Obrigado por escolher nosso banco!") )   
-      console.log(chalk.green('Defina as opções da sua conta a seguir:'))                      
+      console.log(chalk.green('Defina as opções da sua conta a seguir'))  
+      
+      buildAccount()
+}
+
+
+function buildAccount(){
+    inquirer.prompt([{
+        name: 'accountName',
+        message: "Digite um nome para a sua conta:"
+    }]).then((answer) => {
+        //console.log(answer)
+        const accountName = answer['accountName']
+        console.info(answer)
+
+        if(fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+
+        }
+
+        if(fs.existSyncs(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black("Esta conta já existe! Escolha outro nome."))
+            buildAccount()
+            return
+        }
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance":0}', function(err){
+            console.log(err)
+        })
+
+        console.log(chalk.green('Parabéns a sua conta foi criada!'))
+        operation()
+
+
+    }).catch((err) => console.log(err))
 }
